e66c25ba84312ab7c0c529ffb27db4ac193bd971
woniuppp <i@shengxinjing.cn>
2015-12-02 14:45:11 +0800

    1
    1

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -6,12 +6,11 @@ db.autocommit(True)
 cur = db.cursor()
 
 def getMem():
-    f = open('/proc/meminfo')
-    total = int(f.readline().split()[1])
-    free = int(f.readline().split()[1])
-    buffers = int(f.readline().split()[1])
-    cache = int(f.readline().split()[1])
-    f.close()
+    with open('/proc/meminfo') as f:
+        total = int(f.readline().split()[1])
+        free = int(f.readline().split()[1])
+        buffers = int(f.readline().split()[1])
+        cache = int(f.readline().split()[1])
     mem_use = total-free-buffers-cache
     t = int(time.time())
     sql = 'insert into memory (memory,time) value (%s,%s)'%(mem_use/1024,t)
