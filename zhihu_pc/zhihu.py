a56a5f13516e79baa1bf63f90495fd8e27420a61
woniuppp <i@shengxinjing.cn>
2015-12-16 17:00:33 +0800

    1
    1

--- zhihu_pc/zhihu.py
+++ zhihu_pc/zhihu.py
@@ -2,24 +2,31 @@
 import requests
 from pyquery import PyQuery as pq
 import os,sys
-
 reload(sys)
 sys.setdefaultencoding("utf-8")
-url = 'http://zhuanlan.zhihu.com/api/columns/weekly/posts?limit=100'
 
-r = requests.get(url)
-res = '<html><meta charset="utf-8"><link rel="stylesheet" href="http://z1.zhimg.com/styles/5a8458ed.main.css"><body>'
+def getHtml(name):
+	url = 'http://zhuanlan.zhihu.com/api/columns/%s/posts?limit=100' %(name)
 
-res += '<div style="width:1000px" class="main receptacle post-view">'
-for obj in r.json()[::-1]:
-	res+='<div class="entry-content">'
+	r = requests.get(url)
+	res = '<html><meta charset="utf-8"><link rel="stylesheet" href="http://z1.zhimg.com/styles/5a8458ed.main.css"><body>'
 
-	res += '<h1 style="font-size:25px">'+obj['title']+'</h1><hr>'
-	res += obj['publishedTime']
-	res += obj['content']
-	res+='</div>'
-res +="</div>"
-res+='</body></html>'
-with open('res.html','w') as f:
+	res += '<div style="width:1000px" class="main receptacle post-view">'
+	for obj in r.json()[::-1]:
+		res+='<div class="entry-content">'
 
-	f.write(res)
+		res += '<h1 style="font-size:25px">'+obj['title']+'</h1><hr>'
+		res += obj['publishedTime']
+		res += obj['content']
+		res+='</div>'
+	res +="</div>"
+	res+='</body></html>'
+	with open(name+'.html','w') as f:
+
+		f.write(res)
+
+
+zhuanlan_list = ['auxten','daily']
+for name in zhuanlan_list:
+	getHtml(name)
+
